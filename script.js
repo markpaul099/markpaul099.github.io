0f4b3f72e35f48514e9db660986e2d85a2f66536
MarKPauL <markpaulpangan099@gmail.com>
2023-05-06 21:54:03 +0800

    added background image transition

---
+++ script.js
@@ -0,0 +1,46 @@
+var backgroundImage = document.getElementById("background-image");
+var h1 = document.querySelector("#main-content h1");
+
+document.getElementById("button1").addEventListener("mouseover", function() {
+  fadeOutImage();
+  setTimeout(function() {
+    setBackgroundImage("url('images/RIJ_waifu2x_4x_jpg.png')");
+    fadeInImage();
+    h1.style.color = "black";
+    h1.style.textShadow = "2px 2px 2px white"; // Add white text shadow
+  }, 500);
+});
+
+document.getElementById("button1").addEventListener("mouseout", function() {
+  fadeOutImage();
+  h1.style.color = "white";
+  h1.style.textShadow = "none"; // Remove text shadow
+});
+
+document.getElementById("button2").addEventListener("mouseover", function() {
+  fadeOutImage();
+  setTimeout(function() {
+    setBackgroundImage("url('images/TOF_waifu2x_4x_jpg.png')");
+    fadeInImage();
+    h1.style.color = "black";
+    h1.style.textShadow = "2px 2px 2px white"; // Add white text shadow
+  }, 500);
+});
+
+document.getElementById("button2").addEventListener("mouseout", function() {
+  fadeOutImage();
+  h1.style.color = "white";
+  h1.style.textShadow = "none"; // Remove text shadow
+});
+
+function setBackgroundImage(imageUrl) {
+  backgroundImage.style.backgroundImage = imageUrl;
+}
+
+function fadeInImage() {
+  backgroundImage.style.opacity = "1";
+}
+
+function fadeOutImage() {
+  backgroundImage.style.opacity = "0";
+}
