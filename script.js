5a193a7c534734cb031a992e9186c0caa3b05da9
MarKPauL <markpaulpangan099@gmail.com>
2023-05-07 19:37:07 +0800

    preload background image on page startup

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var backgroundImage = document.getElementById("background-image");
 var h1 = document.querySelector("#main-content h1");
 
+// Preload the images
+var image1 = new Image();
+image1.src = "https://hereacti.sirv.com/Images/RIJ_waifu2x_4x_jpg.jpg";
+
+var image2 = new Image();
+image2.src = "https://hereacti.sirv.com/Images/TOF_waifu2x_4x_jpg.jpg";
+
 document.getElementById("button1").addEventListener("mouseover", function() {
   fadeOutImage();
   setTimeout(function() {
